531c6ac81bfc0826022e92f213065a462c16e80c
Larry Villegas <larry_villegas@hotmail.com>
2023-05-09 03:01:34 -0400

    Create TodoApp.jsx

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '@/components/Header';
+import TodosLogic from '@/components/TodosLogic';
+// import Modal from './Modal';
+  
+const TodoApp = () => {
+  return (
+    <div className='wrapper'>
+      <div className='todos'>
+        <Header />
+        {/* <Modal></Modal> */}
+        <TodosLogic />
+      </div>
+    </div>
+  );
+};
+export default TodoApp;
